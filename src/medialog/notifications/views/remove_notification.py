e1f1ce6efac5a8cbb1142add757e42da925bec74
Espen <espen@medialog.no>
2024-11-05 16:06:04 +0000

    "Add subscriber: notification_added"

--- src/medialog/notifications/views/remove_notification.py
+++ src/medialog/notifications/views/remove_notification.py
@@ -2,6 +2,8 @@
 
 from Products.Five.browser import BrowserView
 from zope.interface import Interface
+from plone import api
+from plone.protect.utils import safeWrite
 
 
 class IRemoveNotification(Interface):
@@ -9,8 +11,26 @@ class IRemoveNotification(Interface):
 
 
 class RemoveNotification(BrowserView):
+    
+    def __init__(self, context, request):
+        self.context = context.aq_explicit
+        self.request = request
+        
+
     def __call__(self):
-        template = '''<li class="heading" i18n:translate="">
-          Sample View
-        </li>'''
-        return template
+        context = self.context
+        user = api.user.get_current()
+        item = self.request.get('item')
+        obj  = api.content.get(UID=item) 
+        userid = f'user:{user.id}'
+        if userid in obj.message_users:
+            safeWrite(obj, self.request)
+            obj.message_users = obj.message_users.remove(userid)
+            if obj.message_read == None:
+                [userid]
+            else:    
+                obj.message_read.append(user.id)
+        
+        self.request.response.redirect(self.request.get_header("referer"))
+        # return True
+
