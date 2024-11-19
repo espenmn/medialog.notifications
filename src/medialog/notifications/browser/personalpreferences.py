1671990d8e7298da4a711f128751627ff87924fc
Espen Moe-Nilssen <espen@medialog.no>
2024-11-19 13:32:26 +0100

    Trying to add icons and preferences (backup before testing at home)

---
+++ src/medialog/notifications/browser/personalpreferences.py
@@ -0,0 +1,9 @@
+# from plone.app.users.browser.personalpreferences import PersonalPreferencesPanel
+# from zope.formlib import form
+# from app.statebuttons.userdataschema import IEnhancedUserDataSchema
+
+# class CustomPersonalPreferencesPanel(PersonalPreferencesPanel):
+
+#     form_fields = form.FormFields(IEnhancedUserDataSchema)
+#     # Apply same widget overrides as in the base class
+#     pass
